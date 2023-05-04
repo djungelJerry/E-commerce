c5c97478ca0510019f8b413bc445e3c12aec5492
Alektina Li <alenushka@A-MacBook-Air.local>
2023-05-04 19:26:59 +0200

    Funktioner till varukorgen

--- MVC.E-Commerce/Models/CartItem.cs
+++ MVC.E-Commerce/Models/CartItem.cs
@@ -1,10 +1,11 @@
-﻿namespace MVC.E_Commerce.Models
+﻿
+namespace MVC.E_Commerce.Models
 {
     public class CartItem
     {
         public int Id { get; set; }
         public int ProductId { get; set; }
-        public int Quantity { get; set; }
+        public int Quantity { get; set; }        
         public decimal CartItemTotal { get; set; }
         public int CartId { get; set; }
         public Cart Cart { get; set; }
